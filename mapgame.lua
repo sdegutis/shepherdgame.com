674ec2ebe63857d850ea8821e70901a3af510040
Steven Degutis <sbdegutis@gmail.com>
2024-06-07 08:13:20 -0500

    Experiment...

--- mapgame.lua
+++ mapgame.lua
@@ -1,77 +1,89 @@
 love.graphics.setDefaultFilter("nearest", "nearest")
 local loadP8 = require("pico8")
 
-function love.load()
-  love.physics.setMeter(64)
-  world = love.physics.newWorld(0, 9.81 * 64, true)
-  -- world = love.physics.newWorld(0, 0, true)
+local map = loadP8("sarah/untitled.p8")
+local scaletile = 4
+love.window.setMode(1700, 900)
+love.physics.setMeter(64)
 
-  objects = {}
+local world = love.physics.newWorld(0, 0, true)
 
-  objects.ground1 = {}
-  objects.ground1.body = love.physics.newBody(world, 650 / 2, 650 - 50 / 2)
-  objects.ground1.shape = love.physics.newRectangleShape(650, 25)
-  objects.ground1.fixture = love.physics.newFixture(objects.ground1.body, objects.ground1.shape)
+local objects = {}
 
-  objects.ground2 = {}
-  objects.ground2.body = love.physics.newBody(world, 650 / 2, 50 / 2 + 50)
-  objects.ground2.shape = love.physics.newRectangleShape(650, 25)
-  objects.ground2.fixture = love.physics.newFixture(objects.ground2.body, objects.ground2.shape)
+objects.tiles = {}
 
-  objects.ground3 = {}
-  objects.ground3.body = love.physics.newBody(world, 50 / 2, 650 / 2)
-  objects.ground3.shape = love.physics.newRectangleShape(25, 650)
-  objects.ground3.fixture = love.physics.newFixture(objects.ground3.body, objects.ground3.shape)
+for y = 0, 63 do
+  for x = 0, 127 do
+    local spri = map.map[y][x]
+    if spri ~= 0 then
+      local spr = map.getOrMakeSpriteAt(spri)
+      local tile = {}
 
-  objects.ground4 = {}
-  objects.ground4.body = love.physics.newBody(world, 650 - 50 / 2, 650 / 2)
-  objects.ground4.shape = love.physics.newRectangleShape(25, 650)
-  objects.ground4.fixture = love.physics.newFixture(objects.ground4.body, objects.ground4.shape)
+      local w = scaletile * 8
+      local h = scaletile * 8
 
-  objects.players = {}
-  objects.players[1] = {}
-  objects.players[2] = {}
-  objects.players[3] = {}
-
-  for i = 1, 3 do
-    objects.players[i] = {}
-    objects.players[i].body = love.physics.newBody(world, 650 / 2 + i * 50, 650 / 2, "dynamic")
-    objects.players[i].shape = love.physics.newCircleShape(20)
-    objects.players[i].fixture = love.physics.newFixture(objects.players[i].body, objects.players[i].shape, 1)
-    objects.players[i].fixture:setRestitution(0.9)
-    objects.players[i].points = 0
-  end
-
-  objects.players[1].sprite = loadP8("sarah/untitled.p8").getOrMakeSpriteAt(1);
-  objects.players[2].sprite = loadP8("jane/janegame2.p8").getOrMakeSpriteAt(1);
-  objects.players[3].sprite = loadP8("bomberman.p8").getOrMakeSpriteAt(1);
-
-  objects.block1 = {}
-  objects.block1.body = love.physics.newBody(world, 200, 550, "dynamic")
-  objects.block1.shape = love.physics.newRectangleShape(0, 0, 50, 100)
-  objects.block1.fixture = love.physics.newFixture(objects.block1.body, objects.block1.shape, 5)
-
-  objects.block2 = {}
-  objects.block2.body = love.physics.newBody(world, 200, 400, "dynamic")
-  objects.block2.shape = love.physics.newRectangleShape(0, 0, 100, 50)
-  objects.block2.fixture = love.physics.newFixture(objects.block2.body, objects.block2.shape, 2)
-
-  love.graphics.setBackgroundColor(0.41, 0.53, 0.97)
-  love.window.setMode(650, 650)
-
-  world:setCallbacks(function(fix1, fix2)
-    for i = 1, 3 do
-      if
-          fix2 == objects.players[i].fixture
-          and
-          fix1 == objects.ground2.fixture
-      then
-        local p  = objects.players[i]
-        p.points = p.points + 1
-      end
+      tile.body = love.physics.newBody(world, x * w + 100, y * h + 100, "dynamic")
+      tile.shape = love.physics.newRectangleShape(w, h)
+      tile.fixture = love.physics.newFixture(tile.body, tile.shape)
+      tile.spr = spr
+      table.insert(objects.tiles, tile)
     end
-  end, function() end)
+  end
 end
+
+objects.ground1 = {}
+objects.ground1.body = love.physics.newBody(world, 650 / 2, 650 - 50 / 2)
+objects.ground1.shape = love.physics.newRectangleShape(650, 5)
+objects.ground1.fixture = love.physics.newFixture(objects.ground1.body, objects.ground1.shape)
+
+objects.ground2 = {}
+objects.ground2.body = love.physics.newBody(world, 650 / 2, 50 / 2 + 50)
+objects.ground2.shape = love.physics.newRectangleShape(650, 5)
+objects.ground2.fixture = love.physics.newFixture(objects.ground2.body, objects.ground2.shape)
+
+objects.ground3 = {}
+objects.ground3.body = love.physics.newBody(world, 50 / 2, 650 / 2)
+objects.ground3.shape = love.physics.newRectangleShape(5, 650)
+objects.ground3.fixture = love.physics.newFixture(objects.ground3.body, objects.ground3.shape)
+
+objects.ground4 = {}
+objects.ground4.body = love.physics.newBody(world, 650 - 50 / 2, 650 / 2)
+objects.ground4.shape = love.physics.newRectangleShape(5, 650)
+objects.ground4.fixture = love.physics.newFixture(objects.ground4.body, objects.ground4.shape)
+
+objects.players = {}
+objects.players[1] = {}
+objects.players[2] = {}
+objects.players[3] = {}
+
+for i = 1, 3 do
+  objects.players[i] = {}
+  objects.players[i].body = love.physics.newBody(world, 650 / 2 + i * 50, 650 / 2, "dynamic")
+  objects.players[i].shape = love.physics.newCircleShape(20)
+  objects.players[i].fixture = love.physics.newFixture(objects.players[i].body, objects.players[i].shape, 1)
+  objects.players[i].fixture:setRestitution(0.9)
+  objects.players[i].points = 0
+end
+
+objects.players[1].sprite = loadP8("sarah/untitled.p8").getOrMakeSpriteAt(1);
+objects.players[2].sprite = loadP8("jane/janegame2.p8").getOrMakeSpriteAt(1);
+objects.players[3].sprite = loadP8("bomberman.p8").getOrMakeSpriteAt(1);
+
+objects.block1 = {}
+objects.block1.body = love.physics.newBody(world, 200, 550, "dynamic")
+objects.block1.shape = love.physics.newRectangleShape(0, 0, 50, 100)
+objects.block1.fixture = love.physics.newFixture(objects.block1.body, objects.block1.shape, 5)
+
+objects.block2 = {}
+objects.block2.body = love.physics.newBody(world, 200, 400, "dynamic")
+objects.block2.shape = love.physics.newRectangleShape(0, 0, 100, 50)
+objects.block2.fixture = love.physics.newFixture(objects.block2.body, objects.block2.shape, 2)
+
+love.graphics.setBackgroundColor(0.41, 0.53, 0.97)
+
+
+
+
 
 function love.update(dt)
   world:update(dt)
@@ -114,6 +126,11 @@ function love.draw()
   love.graphics.setColor(0.50, 0.50, 0.50)
   love.graphics.polygon("fill", objects.block1.body:getWorldPoints(objects.block1.shape:getPoints()))
   love.graphics.polygon("fill", objects.block2.body:getWorldPoints(objects.block2.shape:getPoints()))
+
+  love.graphics.setColor(1, 1, 1)
+  for _, tile in pairs(objects.tiles) do
+    tile.spr:draw(tile.body:getX(), tile.body:getY(), scaletile, tile.body:getAngle(), 0)
+  end
 end
 
 -- if pcall(require, "lldebugger") then require("lldebugger").start() end

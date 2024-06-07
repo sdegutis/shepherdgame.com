a4765ff8a91ec2558e1ca2db0ffed98edf9dce1e
Steven Degutis <sbdegutis@gmail.com>
2024-06-07 10:54:09 -0500

    Preparing to try something...

--- mapgame.lua
+++ mapgame.lua
@@ -1,6 +1,8 @@
 love.graphics.setDefaultFilter("nearest", "nearest")
 local loadP8 = require("pico8")
 
+local NP = 2
+
 local W = 1700
 local H = 900
 
@@ -12,7 +14,8 @@ local scaletile = 4
 love.window.setMode(W, H)
 love.physics.setMeter(64)
 
-local world = love.physics.newWorld(0, 0, true)
+-- local world = love.physics.newWorld(0, 0, true)
+local world = love.physics.newWorld(0, 9.81 * 64, true)
 
 local objects = {}
 
@@ -30,7 +33,7 @@ for y = 0, 63 do
 
       tile.body = love.physics.newBody(world, x * w + 100, y * h + 100, "dynamic")
       tile.shape = love.physics.newRectangleShape(w, h)
-      tile.fixture = love.physics.newFixture(tile.body, tile.shape, 3)
+      tile.fixture = love.physics.newFixture(tile.body, tile.shape, 1)
       tile.spr = spr
       table.insert(objects.tiles, tile)
     end
@@ -61,7 +64,7 @@ objects.players[1].sprite = loadP8("sarah/untitled.p8").getOrMakeSpriteAt(1);
 objects.players[2].sprite = loadP8("jane/janegame2.p8").getOrMakeSpriteAt(1);
 objects.players[3].sprite = loadP8("bomberman.p8").getOrMakeSpriteAt(1);
 
-love.graphics.setBackgroundColor(0.41, 0.53, 0.97)
+love.graphics.setBackgroundColor(0.1, 0.1, 0.3)
 
 function love.update(dt)
   world:update(dt)
@@ -69,18 +72,22 @@ function love.update(dt)
   --- @type love.Joystick[]
   local joysticks = love.joystick.getJoysticks()
 
-  for i = 1, 3 do
+  for i = 1, NP do
     local x, y = joysticks[i]:getAxes()
     x = x or 0
     y = y or 0
     objects.players[i].body:applyForce(x * 300, y * 300)
+
+    if joysticks[i]:isDown(1) then
+      objects.players[i].body:applyForce(0, -1000)
+    end
   end
 end
 
 function love.draw()
   love.graphics.setColor(1, 1, 1)
 
-  for i = 1, 3 do
+  for i = 1, NP do
     local body = objects.players[i].body
     -- local shape = objects.players[i].shape
     -- love.graphics.polygon("fill", body:getWorldPoints(shape:getPoints()))

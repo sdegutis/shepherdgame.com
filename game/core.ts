da18d7e354f654351eca1b787ed7533752d4ead7
sdegutis <sbdegutis@gmail.com>
2024-06-24 08:39:54 -0500

    Ready.

--- game/core.ts
+++ game/core.ts
@@ -1,3 +1,5 @@
+import { loadP8 } from "./pico8.js";
+
 export function runGameLoop() {
   const engine = { update: () => { } };
   const framerate = 1000 / 30;
@@ -23,4 +25,35 @@ export function createCanvas(width: number, height: number, scale: number) {
   canvas.height = height / scale;
   document.body.append(canvas);
   return canvas.getContext('2d')!;
+}
+
+export function getPlayers(engine: { update: () => void }, ctx: CanvasRenderingContext2D) {
+  return new Promise<void>(async resolve => {
+    const core = await loadP8('game/core.p8');
+
+    engine.update = () => {
+      const gamepads = navigator.getGamepads();
+
+      ctx.reset();
+
+      for (let i = 0; i < 4; i++) {
+        const gamepad = gamepads[i];
+
+        const x = i * 30 + 120
+        const w = 4;
+
+        ctx.strokeStyle = '#fff';
+        ctx.lineWidth = 3;
+        ctx.fillStyle = gamepad ? '#9f9' : '#f77';
+        ctx.fillRect(x - w, 100 - w, 8 + (w * 2), 8 + (w * 2));
+
+        const spr = core.sprites[1]!;
+        ctx.putImageData(spr, x, 100);
+
+        if (gamepad?.buttons[9]?.pressed) {
+          resolve();
+        }
+      }
+    }
+  });
 }
--- game/main.ts
+++ game/main.ts
@@ -1,4 +1,4 @@
-import { createCanvas, runGameLoop } from "./core.js";
+import { createCanvas, getPlayers, runGameLoop } from "./core.js";
 import { loadP8 } from "./pico8.js";
 
 // sarahs idea:
@@ -9,7 +9,7 @@ import { loadP8 } from "./pico8.js";
 
 const ctx = createCanvas(1400, 900, 4);
 const engine = runGameLoop();
-await getPlayers();
+await getPlayers(engine, ctx);
 
 
 const game1 = await loadP8('game/explore.p8');
@@ -29,22 +29,21 @@ enum B {
 }
 
 engine.update = () => {
-  const players = navigator.getGamepads();
-  for (const player of players) {
-    if (!player) continue;
+  for (const gamepad of navigator.getGamepads()) {
+    if (!gamepad) continue;
 
-    player.vibrationActuator!.reset();
-    if (player.buttons[B.ZR]!.value || player.buttons[B.ZL]!.value) {
-      player.vibrationActuator!.playEffect("dual-rumble", {
+    gamepad.vibrationActuator.reset();
+    if (gamepad.buttons[B.ZR]!.value || gamepad.buttons[B.ZL]!.value) {
+      gamepad.vibrationActuator.playEffect("dual-rumble", {
         startDelay: 0,
         duration: 500,
-        weakMagnitude: player.buttons[B.ZL]!.value,
-        strongMagnitude: player.buttons[B.ZR]!.value,
+        weakMagnitude: gamepad.buttons[B.ZL]!.value,
+        strongMagnitude: gamepad.buttons[B.ZR]!.value,
       });
     }
 
 
-    const [x1, y1, x2, y2] = player.axes as [number, number, number, number];
+    const [x1, y1, x2, y2] = gamepad.axes as [number, number, number, number];
 
     mx += x1;
     my += y1;
@@ -71,35 +70,4 @@ function draw() {
       }
     }
   }
-}
-
-function getPlayers() {
-  return new Promise<void>(async resolve => {
-    const core = await loadP8('game/core.p8');
-
-    engine.update = () => {
-      const gamepads = navigator.getGamepads();
-
-      ctx.reset();
-
-      for (let i = 0; i < 4; i++) {
-        const gamepad = gamepads[i];
-
-        const x = i * 30 + 120
-        const w = 4;
-
-        ctx.strokeStyle = '#fff';
-        ctx.lineWidth = 3;
-        ctx.fillStyle = gamepad ? '#9f9' : '#f77';
-        ctx.fillRect(x - w, 100 - w, 8 + (w * 2), 8 + (w * 2));
-
-        const spr = core.sprites[1]!;
-        ctx.putImageData(spr, x, 100);
-
-        if (gamepad?.buttons[9]?.pressed) {
-          resolve();
-        }
-      }
-    }
-  });
 }

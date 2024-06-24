7fa22b0bd9dfa0255a5f57100faff2d3953faa71
sdegutis <sbdegutis@gmail.com>
2024-06-24 08:20:39 -0500

    Hmm...

--- game/main.ts
+++ game/main.ts
@@ -8,10 +8,82 @@ import { loadP8 } from "./pico8.js";
 
 
 const ctx = createCanvas(1400, 900, 4);
-
 const engine = runGameLoop();
-
 await getPlayers();
+
+
+const game1 = await loadP8('game/explore.p8');
+
+let mx = 0;
+let my = 0;
+
+draw();
+
+const BUTTONS = [
+  'A', 'B', 'X', 'Y',
+  'L', 'R', 'ZL', 'ZR',
+  '-', '+',
+  'LTRIGGER', 'RTRIGGER',
+  'UP', 'DOWN', 'LEFT', 'RIGHT',
+  'HOME',
+] as const;
+
+type Buttons = { [key in typeof BUTTONS[number]]: GamepadButton };
+
+const ZL = 6;
+const ZR = 7;
+
+// const players = navigator.getGamepads().filter(c => c !== null).map(gamepad => {
+//   const buttons: Buttons = Object.fromEntries(gamepad.buttons.map((b, i) => [BUTTONS[i], b]));
+//   return { gamepad, buttons };
+// });
+
+engine.update = () => {
+  const players = navigator.getGamepads();
+  for (const player of players) {
+    if (!player) continue;
+
+    console.log(player.axes)
+
+    player.vibrationActuator!.reset();
+    if (player.buttons[ZR]!.value || player.buttons[ZL]!.value) {
+      player.vibrationActuator!.playEffect("dual-rumble", {
+        startDelay: 0,
+        duration: 500,
+        weakMagnitude: player.buttons[ZL]!.value,
+        strongMagnitude: player.buttons[ZR]!.value,
+      });
+    }
+
+
+    const [x1, y1, x2, y2] = player.axes as [number, number, number, number];
+
+    mx += x1;
+    my += y1;
+
+    mx += x2 * 3;
+    my += y2 * 3;
+
+    draw();
+  }
+
+
+};
+
+
+function draw() {
+  ctx.reset();
+
+  for (let y = 0; y < 64; y++) {
+    for (let x = 0; x < 128; x++) {
+      const spr = game1.map[y]![x]!;
+      if (spr > 0) {
+        const img = game1.sprites[spr]!;
+        ctx.putImageData(img, mx + x * 8, my + y * 8);
+      }
+    }
+  }
+}
 
 function getPlayers() {
   return new Promise<void>(async resolve => {
@@ -40,75 +112,6 @@ function getPlayers() {
           resolve();
         }
       }
-
     }
   });
-}
-
-
-
-
-
-
-const game1 = await loadP8('game/explore.p8');
-
-let mx = 0;
-let my = 0;
-
-draw();
-
-const BUTTONS = [
-  'A', 'B', 'X', 'Y',
-  'L', 'R', 'ZL', 'ZR',
-  '-', '+',
-  'LTRIGGER', 'RTRIGGER',
-  'UP', 'DOWN', 'LEFT', 'RIGHT',
-  'HOME',
-] as const;
-
-type Buttons = { [key in typeof BUTTONS[number]]: GamepadButton };
-
-engine.update = () => {
-
-  const c = navigator.getGamepads()[1];
-  if (!c) return;
-
-  const buttons: Buttons = Object.fromEntries(c.buttons.map((b, i) => [BUTTONS[i], b]));
-
-  c.vibrationActuator!.reset();
-  if (buttons.ZR.value || buttons.ZL.value) {
-    c.vibrationActuator!.playEffect("dual-rumble", {
-      startDelay: 0,
-      duration: 500,
-      weakMagnitude: buttons.ZL.value,
-      strongMagnitude: buttons.ZR.value,
-    });
-  }
-
-
-  const [x1, y1, x2, y2] = c.axes as [number, number, number, number];
-
-  mx += x1;
-  my += y1;
-
-  mx += x2 * 3;
-  my += y2 * 3;
-
-  draw();
-
-};
-
-
-function draw() {
-  ctx.reset();
-
-  for (let y = 0; y < 64; y++) {
-    for (let x = 0; x < 128; x++) {
-      const spr = game1.map[y]![x]!;
-      if (spr > 0) {
-        const img = game1.sprites[spr]!;
-        ctx.putImageData(img, mx + x * 8, my + y * 8);
-      }
-    }
-  }
 }

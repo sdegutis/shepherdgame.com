b16c3e6b9e52b787d7cf24cb8c50efe9620c5be8
sdegutis <sbdegutis@gmail.com>
2024-06-25 07:44:29 -0500

    Cleaner.

--- game/main.ts
+++ game/main.ts
@@ -9,14 +9,16 @@ import { loadCleanP8 } from "./pico8.js";
 const WIDTH = 1500;
 const HEIGHT = 900;
 
-const ctx = createCanvas(WIDTH, HEIGHT, 4);
+const ctx = createCanvas(WIDTH, HEIGHT, 2);
 const engine = runGameLoop();
-const playerIndexes = await getPlayers(engine, ctx);
+const gamepadIndexes = await getPlayers(engine, ctx);
 
 
 const game1 = await loadCleanP8('game/explore.p8');
 
-class Drawable {
+class Entity {
+
+  layer = 0;
 
   constructor(
     public x: number,
@@ -24,85 +26,71 @@ class Drawable {
     public image: OffscreenCanvas,
   ) { }
 
-}
-
-class Player extends Drawable {
-
-  gamepadIndex;
-
-  constructor(
-    x: number,
-    y: number,
-    image: OffscreenCanvas,
-  ) {
-    super(x, y, image);
-    this.gamepadIndex = playerIndexes.shift()!;
-    console.log(this.gamepadIndex)
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.drawImage(this.image, Math.round(this.x), Math.round(this.y));
   }
 
+}
+
+class Player {
+
+  gamepadIndex = gamepadIndexes.shift()!;;
   get gamepad() { return navigator.getGamepads()[this.gamepadIndex]! }
+
+  constructor(public entity: Entity) {
+  }
 
   update() {
     const [x1, y1] = this.gamepad.axes;
-    this.x += x1;
-    this.y += y1;
+    this.entity.x += x1;
+    this.entity.y += y1;
   }
 
+  // gamepad.vibrationActuator.reset();
+  // if (gamepad.buttons[ZR].value || gamepad.buttons[ZL].value) {
+  //   gamepad.vibrationActuator.playEffect("dual-rumble", {
+  //     startDelay: 0,
+  //     duration: 500,
+  //     weakMagnitude: gamepad.buttons[ZL].value,
+  //     strongMagnitude: gamepad.buttons[ZR].value,
+  //   });
+  // }
+
 }
 
-interface Drawable {
-  image: OffscreenCanvas;
-  x: number;
-  y: number;
-}
-
-const drawables: Drawable[] = [];
+const entities: Entity[] = [];
 const players: Player[] = [];
 
 for (let y = 0; y < 64; y++) {
   for (let x = 0; x < 128; x++) {
     const tile = game1.map[y][x];
 
-    const image = tile.sprite.image;
-    const px = x * 8;
-    const py = y * 8;
+    const drawable = new Entity(x * 8, y * 8, tile.sprite.image);
+    entities.push(drawable);
 
     if (tile.index >= 1 && tile.index <= 3) {
-      const entity = new Player(px, py, image);
-      players.push(entity);
-    }
-    else {
-      const entity = new Drawable(px, py, image);
-      drawables.push(entity);
+      const player = new Player(drawable);
+      drawable.layer = 1;
+      players.push(player);
     }
   }
 }
 
+entities.sort((a, b) => {
+  if (a.layer > b.layer) return 1;
+  if (a.layer < b.layer) return -1;
+  return 0;
+});
 
 engine.update = () => {
   for (const player of players) {
     player.update();
-
-    // gamepad.vibrationActuator.reset();
-    // if (gamepad.buttons[ZR].value || gamepad.buttons[ZL].value) {
-    //   gamepad.vibrationActuator.playEffect("dual-rumble", {
-    //     startDelay: 0,
-    //     duration: 500,
-    //     weakMagnitude: gamepad.buttons[ZL].value,
-    //     strongMagnitude: gamepad.buttons[ZR].value,
-    //   });
-    // }
   }
 
   ctx.reset();
   // ctx.translate(Math.round(mx), Math.round(my));
 
-  for (const d of drawables) {
-    ctx.drawImage(d.image, d.x, d.y);
+  for (const e of entities) {
+    e.draw(ctx);
   }
-
-  for (const p of players) {
-    ctx.drawImage(p.image, Math.round(p.x), Math.round(p.y));
-  }
-
 };

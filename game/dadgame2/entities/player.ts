2106f67366c2b3d8f5a476a9607796fa19ff6cf5
sdegutis <sbdegutis@gmail.com>
2024-07-09 10:48:10 -0500

    Unsure.

--- game/dadgame2/entities/player.ts
+++ game/dadgame2/entities/player.ts
@@ -17,7 +17,7 @@ export class Player implements Updatable, Actable {
 
   stoodFor = 0;
 
-  hasWand = true;
+  hasWand = false;
   bubble: Bubble | undefined;
 
   constructor(public entity: Entity) { }

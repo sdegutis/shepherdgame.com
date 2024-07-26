3e3bd4433973f11f67feae2ca176f30038001bfa
sdegutis <sbdegutis@gmail.com>
2024-07-26 11:48:19 -0500

    Declutter.

--- src/entities/player.ts
+++ src/entities/player.ts
@@ -17,25 +17,6 @@ export class Player extends Entity {
     game.players[playerNum] = this;
   }
 
-  override draw(layer: number): void {
-    super.draw(layer);
-
-    // if (layer === 2) {
-    //   const cx = Math.round(this.x + (this.image.w / 2) - game.camera.x);
-    //   const cy = Math.round(this.y + (this.image.h / 2) - game.camera.y);
-
-    //   for (let x = -10; x < 10; x++) {
-    //     for (let y = -10; y < 10; y++) {
-    //       const d = Math.sqrt((x ** 2) + (y ** 2));
-    //       if (d < 10) {
-    //         const i = (cy + y) * 320 * 4 + (cx + x) * 4;
-    //         game.pixels[i + 3] = 255;
-    //       }
-    //     }
-    //   }
-    // }
-  }
-
 }
 
 

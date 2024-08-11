cc5cbbec8eab9ad348f5dff42ab6808d6baa8516
sdegutis <sbdegutis@gmail.com>
2024-08-11 10:17:25 -0500

    Remove useless file.

--- site/game.ts
+++ site/game.ts
@@ -163,9 +163,25 @@ export class Game {
 
 }
 
-export async function startGame() {
-  const crt = setupCRT();
-  const game = new Game(crt);
-  await game.load();
-  game.start();
-}
+const crt = setupCRT();
+const game = new Game(crt);
+await game.load();
+game.start();
+
+// plans:
+
+// 0 = night
+// 1 = water
+// 2 = leaves
+// 3 = grass
+// 4 = dirt
+// 5 = rocky
+// 6 = ice
+// 7 = snow
+// 8 = red leaves?
+// 9 = magma
+// 10 = autumn grass
+// 11 = light grass
+// 12 = shallow water
+// 13 = concrete?
+// 15 = sand
